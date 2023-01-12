c2b08b66951e1e8056bced56d8505ce8d48baef4
José Weliton Silva dos Santos <welitonkira2gmail.com>
2023-01-12 12:31:41 -0300

    Correction in the constructor of the SalesRecord class

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -15,10 +15,10 @@ namespace SalesWebMvc.Models
         {
         }
 
-        public SalesRecord(int id, DateTime birthDate, double amount, SaleStatus status, Seller seller)
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
         {
             Id = id;
-            Date = birthDate;
+            Date = date;
             Amount = amount;
             Status = status;
             Seller = seller;
